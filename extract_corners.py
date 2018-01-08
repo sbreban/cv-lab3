887850e7639f2fa3b2efa0fa551c56a90d081170
Sergiu Breban <breban.sergiu94@gmail.com>
2018-01-08 20:20:29 +0200

    Remove comments

--- extract_corners.py
+++ extract_corners.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
 
-# img = cv2.imread('MorpologicalCornerDetection.png', 0)
-img = cv2.imread('square-rectangle.png', 0)
+img = cv2.imread('MorpologicalCornerDetection.png', 0)
+# img = cv2.imread('square-rectangle.png', 0)
 
 cross = np.array([
     [0, 0, 1, 0, 0],
@@ -34,16 +34,14 @@ square = np.array([
 r2 = cv2.dilate(img, xshape, iterations=1)
 r2 = cv2.erode(r2, square, iterations=1)
 r = cv2.absdiff(r2, r1)
-# grab the image dimensions
+
 h = r.shape[0]
 w = r.shape[1]
-
-# loop over the image, pixel by pixel
 for x in range(0, h):
     for y in range(0, w):
-        # threshold the pixel
         if r[x, y] > 50:
             cv2.circle(img, (y, x), 5, (255, 0, 0))
+
 cv2.imshow('image', img)
 k = cv2.waitKey(0) & 0xFF
 if k == 27:  # wait for ESC key to exit
